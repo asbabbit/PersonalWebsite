d373c2768a04347e6c5a9253ed24972704f1fabe
Runny-Babbitt <81458205+Runny-Babbitt@users.noreply.github.com>
2025-02-17 19:25:40 -0700

    Fixed relative paths

--- src/app/_services/project.service.ts
+++ src/app/_services/project.service.ts
@@ -15,7 +15,7 @@ export class ProjectService {
       description: 'Test Description',
       projectLink: 'https://github.com',
       tags: [Tag.CPP, Tag.PYTHON],
-      pictures: ["../../assets/Grad.JPG","../../assets/Grad.JPG","../../assets/Grad.JPG"]
+      pictures: ["assets/Grad.JPG","/assets/Grad.JPG","assets/Grad.JPG"]
     },
     {
       id: 1,
@@ -24,7 +24,7 @@ export class ProjectService {
       description: 'Test Description',
       projectLink: 'https://github.com',
       tags: [Tag.CPP, Tag.VERILOG],
-      pictures: ["../../assets/ProfileSqr.JPG","../../assets/Grad.JPG","../../assets/ProfileSqr.JPG"]
+      pictures: ["assets/ProfileSqr.JPG","assets/Grad.JPG","assets/ProfileSqr.JPG"]
     },
     {
       id: 2,
