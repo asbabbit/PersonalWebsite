191aae69c75772d7cb5f85fcc14b9e319ac1763f
Runny-Babbitt <81458205+Runny-Babbitt@users.noreply.github.com>
2025-02-21 12:57:46 -0700

    Added P2P project

--- src/app/_services/project.service.ts
+++ src/app/_services/project.service.ts
@@ -10,12 +10,12 @@ export class ProjectService {
     projects: Project[] = [
       {
       id: 0,
-      name: 'Sample',
-      summary: 'Test Summary',
-      description: 'Test Description',
-      projectLink: 'https://github.com',
+      name: 'Distributed Hash Table P2P Network',
+      summary: "Created a DHT for storing entries in a table amongst a host of peers managed by the DHT manager.",
+      description: 'I developed an application where processes communicate using sockets to build and query a Distributed Hash Table (DHT). The system uses a ring topology for DHT management and implements hot potato query processing. The project involved creating two main components: An \"always-on\" manager program that manages the DHT peers, listens on a specified port, and supports communication between peers. A peer program that interacts with the manager for DHT construction, management, and query processing. I used socket programming to implement these components, ensuring that the communication between peers followed a hot potato protocol. The manager oversees the DHT creation and manages peer churn. Throughout the project, I utilized version control to manage the code and regularly committed updates to a private repository.',
+      projectLink: 'https://github.com/asbabbit/SocketProject/tree/main',
       tags: [Tag.CPP, Tag.PYTHON],
-      pictures: ["assets/Grad.JPG","/assets/Grad.JPG","assets/Grad.JPG"]
+      pictures: ["assets/DHT.png"]
     },
     {
       id: 1,
